bc0994e49ae9ee6fa27ef99fa81982adb832fc4c
Dania Rachid <rachidd-wp18@student.tarc.edu.my>
2022-04-04 23:34:30 +0300

    sign in with Email and password

--- lib/utils/authentication.dart
+++ lib/utils/authentication.dart
@@ -11,16 +11,6 @@ class Authentication {
 
 
 
-  static void checkSignedIn(BuildContext context) {
-    Authentication.initializeFirebase();
-    //if logged in
-    User? user = FirebaseAuth.instance.currentUser;
-    if (user != null) {
-      Navigator.of(context).pushReplacement(
-          MaterialPageRoute(builder: (context) => HomePage())
-      );
-    }
-  }
   static Future<FirebaseApp> initializeFirebase() async {
     FirebaseApp firebaseApp = await Firebase.initializeApp();
     return firebaseApp;
@@ -70,10 +60,6 @@ class Authentication {
 
   }
 
-  static Future<void> signout({required BuildContext context}) async {
-    await FirebaseAuth.instance.signOut();
-  }
-
   static void signup(BuildContext context,String email, String password) {
     FirebaseAuth auth = FirebaseAuth.instance;
     auth.createUserWithEmailAndPassword(
@@ -96,8 +82,5 @@ class Authentication {
     });
   }
 
-  static void resetPassword(BuildContext context, String email) {
-    final auth = FirebaseAuth.instance;
-    auth.sendPasswordResetEmail(email: email);
-  }
+
 }
